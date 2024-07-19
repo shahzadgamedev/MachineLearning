feb23de3a3345be0e0b91e5bc2fefc0ec4d96b52
Shahzad Ansari <95477157+shahzadgamedev@users.noreply.github.com>
2024-07-19 10:52:13 +0530

    First Commit - BasicFunctions.py Contains basic logic for model and cost function - Salary_Data.csv Contains Simple Data for Doing Linear Regression. - SalaryExample.ipynb Linear Regression Example

---
+++ SupervisedLearning/Regression/BasicFunctions.py
@@ -0,0 +1,14 @@
+﻿import numpy as np
+
+
+def model(input_x, weight, bias):
+    return input_x * weight + bias
+
+
+def squared_error_cost(targets, predictions):
+    samples = len(targets)
+
+    accumulated_error = 0.0
+    for target, prediction in zip(targets, predictions):
+        accumulated_error += (target - prediction) ** 2
+    return (1.0 / (2 * samples)) * accumulated_error
