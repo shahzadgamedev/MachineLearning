651791210060ee31dfc94ee4821dcc60f5fb49e3
Shahzad Ansari <95477157+shahzadgamedev@users.noreply.github.com>
2024-07-20 13:35:12 +0530

    Added Gradient Descent - Added Two Extra Plots for Weight Vs Cost and Weight vs bias to show the results clearly - 3d plot is now rotated and colored - cost vs Iteration in Gradient Descent

--- SupervisedLearning/Regression/BasicFunctions.py
+++ SupervisedLearning/Regression/BasicFunctions.py
@@ -1,16 +1,38 @@
 ﻿import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 
-def model(input_x, weight, bias):
-    return input_x * weight + bias
+class BasicFunctions:
+    def __init__(self):
+        pass
 
+    @staticmethod
+    def model(input_x, weight, bias):
+        return input_x * weight + bias
 
-def squared_error_cost(targets, predictions):
-    samples = len(targets)
+    @staticmethod
+    def squared_error_cost(targets, predictions):
+        samples = len(targets)
 
-    accumulated_error = 0.0
-    for target, prediction in zip(targets, predictions):
-        accumulated_error += (target - prediction) ** 2
-    return (1.0 / (2 * samples)) * accumulated_error
+        accumulated_error = 0.0
+        for target, prediction in zip(targets, predictions):
+            accumulated_error += (target - prediction) ** 2
+        return (1.0 / (2 * samples)) * accumulated_error
 
+    @staticmethod
+    def gradient_weight(x, y, w, b) -> float:
+        m = len(x)
+        accumulated_error = 0.0
+        for i in range(m):
+            accumulated_error += (BasicFunctions.model(x[i], w, b) - y[i]) * x[i]
+        return accumulated_error / m
 
+    @staticmethod
+    def gradient_bias(x, y, w, b) -> float:
+        m = len(x)
+        accumulated_error = 0.0
+        for i in range(m):
+            accumulated_error += BasicFunctions.model(x[i], w, b) - y[i]
+        return accumulated_error / m
