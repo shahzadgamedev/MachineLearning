22f8a10fcabf063762f52b5bc223338f959f4e36
Shahzad Ansari <95477157+shahzadgamedev@users.noreply.github.com>
2024-07-25 19:56:55 +0530

    Added Linear Regression with Multiple Features Updated HousingPriceExample.ipynb

--- SupervisedLearning/Regression/BasicFunctions.py
+++ SupervisedLearning/Regression/BasicFunctions.py
@@ -1,7 +1,4 @@
 ﻿import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
 
 
 class BasicFunctions:
@@ -15,24 +12,5 @@ class BasicFunctions:
     @staticmethod
     def squared_error_cost(targets, predictions):
         samples = len(targets)
-
-        accumulated_error = 0.0
-        for target, prediction in zip(targets, predictions):
-            accumulated_error += (target - prediction) ** 2
-        return (1.0 / (2 * samples)) * accumulated_error
-
-    @staticmethod
-    def gradient_weight(x, y, w, b) -> float:
-        m = len(x)
-        accumulated_error = 0.0
-        for i in range(m):
-            accumulated_error += (BasicFunctions.model(x[i], w, b) - y[i]) * x[i]
-        return accumulated_error / m
-
-    @staticmethod
-    def gradient_bias(x, y, w, b) -> float:
-        m = len(x)
-        accumulated_error = 0.0
-        for i in range(m):
-            accumulated_error += BasicFunctions.model(x[i], w, b) - y[i]
-        return accumulated_error / m
+        cost = np.sum((targets - predictions) ** 2)
+        return (1.0 / (2 * samples)) * cost
